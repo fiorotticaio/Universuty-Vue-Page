aff78493e2f7534ab72572883131dc35be29c4be
fiorotticaio <caiofiorotti@gmail.com>
2022-07-26 13:36:08 -0300

    backend and frontend separeted

---
+++ frontend/src/components/EditCourse.vue
@@ -0,0 +1,111 @@
+<template>
+    
+    <div>
+        <h2>Editar curso</h2>
+
+        <label for="select-couse">Selecione um curso</label>
+        <select id="sel-course" name="sel-course">
+            <option value="selectCard">Selecione uma opção</option>
+            <option value="engComp">Engenharia da computação</option>
+            <option value="artes">Artes</option>
+            <option value="mat">Matemática</option>
+            <option value="filo">Filosofia</option>
+        </select>
+
+        <div v-if="checkSelectCourse()" class="course-data">
+            <label for="name">Nome</label>
+            <input type="text" id="name" name="courseName" placeholder="{{ Nome do curso }}">
+    
+            <label for="id">ID</label>
+            <input type="text" id="id" name="courseID" placeholder="{{ ID do curso }}">
+    
+            <label for="code">ID</label>
+            <input type="text" id="code" name="courseCode" placeholder="{{ Código do curso }}">
+    
+            <label for="area">Área do conhecimento</label>
+            <select class="data" id="area" name="area">
+                <option value="exatas">Exatas</option>
+                <option value="biomedicas">Biomédicas</option>
+                <option value="humanas">Humanas</option>
+            </select>
+
+        </div>
+
+    </div>
+    
+</template>
+
+
+<script>
+
+function checkSelectCourse() {
+    // var card = document.getElementByName("sel-course")[0].value;
+    // if (card.value == selectCard) {
+    //     return 0;
+    // } else {
+    //     return 1;
+    // }
+}
+
+export default {
+    name: 'EditCourse',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        checkSelectCourse,
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+
+div {
+    font-size: 5rem;
+
+    background-color: #2980b9;
+    border: 1px solid #222f3e;
+    max-width: 70%;
+    min-height: 30rem;
+
+    margin: 2rem auto;
+    padding: 2rem;
+    border-radius: 2rem;
+    box-shadow: #222f3e 0 0 0.6rem;
+
+}
+
+div h2 {
+    font-family: Montserrat;
+    font-weight: 400;
+    color: #24282c; 
+    text-decoration: underline;
+    text-align: center;
+}
+
+input[type=text], select.data {
+    width: 100%;
+    padding: 1.2rem 2.0rem;
+    margin: 0.8rem 0;
+    display: inline-block;
+    border: 0.1rem solid #ccc;
+    border-radius: 0.4rem;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 1.4rem 2.0rem;
+    margin: 0.8rem 0;
+    border: none;
+    border-radius: 0.4rem;
+    cursor: pointer;
+}
+
+
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <header>
+        <img src="../assets/logo-ufes.png" />
+        <nav>
+            <ul>
+                <li><a href="">LISTAR</a></li>
+                <li><a href="">CRIAR</a></li>
+                <li><a href="">EDITAR</a></li>
+                <li><a href="">DELETAR</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+    data() {
+        return {
+        
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+
+header {
+    background: linear-gradient(to right, #54a0ff, #0abde3);
+    height: 10rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0 2rem;
+}
+
+img {
+    width: 10rem;;
+    height: auto;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+    margin: 5rem;
+    padding: 0;
+    gap: 2rem;
+}
+
+a {
+    font-size: 2rem;
+    font-family: Montserrat;
+    font-weight: 400;
+    color: #222f3e; 
+    text-decoration: none;
+    
+    transition: all 0.4s;
+}
+
+a:hover {
+    font-weight: 700;;
+}
+
+</style>
